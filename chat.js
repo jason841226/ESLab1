8928fa2aa849b652f217b5252b386cd9a7c08314
jason841226 <b03901053@ntu.edu.tw>
2018-03-31 19:08:23 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -10,8 +10,22 @@ var config = {
 firebase.initializeApp(config);
 
 
-
-alert("aaa");
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+		// User is signed in.
+		var displayName = user.displayName;
+		var email = user.email;
+		var emailVerified = user.emailVerified;
+		var photoURL = user.photoURL;
+		var isAnonymous = user.isAnonymous;
+		var uid = user.uid;
+		var providerData = user.providerData;
+		// ...
+	} else {
+		// User is signed out.
+		// ...
+	}
+});
 
 
 var rootRef = firebase.database().ref().child('messages')

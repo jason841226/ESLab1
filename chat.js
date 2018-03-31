742773ecb550b7013eade9ff43e5d32ed4bb8b13
jason841226 <b03901053@ntu.edu.tw>
2018-03-31 18:22:24 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -18,8 +18,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     var isAnonymous = user.isAnonymous;
     var uid = user.uid;
     var providerData = user.providerData;
-	  
-    // ...
+    byId("user").innerHTML=email
   } else {
     // User is signed out.
     // ...

9db56a210fedd40c6ced66a4dd786169ab2cda2f
Shigurefox <shigurefox@gmail.com>
2018-03-31 19:58:24 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -13,6 +13,16 @@ var user = firebase.auth().currentUser;
 
 if (user != null) {
   // User is signed in.
+  var rootRef = firebase.database().ref().child('messages')
+  rootRef.on('child_added',function(snapshot){
+	var row = byId("myTable").insertRow(-1);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	cell1.innerHTML = snapshot.val().user;
+	cell2.innerHTML = snapshot.val().message;
+	cell3.innerHTML = snapshot.val().time;
+  })
 
   byId("send_button").addEventListener("click", function(){
       rootRef.push({
@@ -28,16 +38,3 @@ if (user != null) {
   alert("You are not logged in.")
   window.location.replace("https://jason841226.github.io/ESLab1/login.html");
 }
-
-
-
-var rootRef = firebase.database().ref().child('messages')
-rootRef.on('child_added',function(snapshot){
-	var row = byId("myTable").insertRow(-1);
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
-	var cell3 = row.insertCell(2);
-	cell1.innerHTML = snapshot.val().user;
-	cell2.innerHTML = snapshot.val().message;
-	cell3.innerHTML = snapshot.val().time;
-})

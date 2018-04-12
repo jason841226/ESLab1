24ed03899d69514689192bf60c5a882726f084f7
jason841226 <b03901053@ntu.edu.tw>
2018-04-13 01:29:48 +0800

    Add files via upload

--- util.js
+++ util.js
@@ -1,13 +1,4 @@
 var byId = function(id){return document.getElementById(id);};
-function startTime() {
-    var d = new Date();
-    var h = checkTime(d.getHours());
-    var m = checkTime(d.getMinutes());
-    var s = checkTime(d.getSeconds());
-    var c = document.querySelector('clock');
-    c.innerHTML=[h,m,s].join(':');
-    setTimeout(startTime, 100);
-}
 function getTime() {
 	var d = new Date();
     var h = checkTime(d.getHours());
@@ -18,21 +9,14 @@ function getTime() {
 function checkTime(i) {
 	return i < 10 ? '0'+i : i;
 }
-
 function getPage(page){
     $(document).ready(function(){
         $.ajax({
             url: ("https://jason841226.github.io/ESLab1/"+page),
             asycn:true,
             success:function(data){
-                //var content = $($.parseHTML(data, document, true));
                 $('#content').html(data);
-                eval($(data).find("script").text());
             }
         });
-        //$.getScript("somescript.js", function(){
-        //    alert("Script loaded and executed.");
-        //});
-        //$("#content").html(htmlobj.responseText);
     });
 }

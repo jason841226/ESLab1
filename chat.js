fe317bc36acef407aa308f083e64e3883680338d
jason841226 <b03901053@ntu.edu.tw>
2018-04-13 00:59:32 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,3 +1,12 @@
+var config = {
+  apiKey: "AIzaSyD2PqrzzHqV3ufERgB9PNlk9D1ciyQ6S1I",
+  authDomain: "eslab1-92fd6.firebaseapp.com",
+  databaseURL: "https://eslab1-92fd6.firebaseio.com",
+  projectId: "eslab1-92fd6",
+  storageBucket: "eslab1-92fd6.appspot.com",
+  messagingSenderId: "141786087397"
+};
+firebase.initializeApp(config);
 var user = firebase.auth().currentUser;
 
 if (user != null) {

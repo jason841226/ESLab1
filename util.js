e029c8820ad29f94073b92a7dc4a6cc301a1650b
jason841226 <b03901053@ntu.edu.tw>
2018-04-13 00:02:24 +0800

    Add files via upload

--- util.js
+++ util.js
@@ -1,29 +1,38 @@
 var byId = function(id){return document.getElementById(id);};
 function startTime() {
-	var d = new Date();
-	var h = checkTime(d.getHours());
-	var m = checkTime(d.getMinutes());
-	var s = checkTime(d.getSeconds());
-	var c = document.querySelector('clock');
-	c.innerHTML=[h,m,s].join(':');
-	setTimeout(startTime, 100);
+    var d = new Date();
+    var h = checkTime(d.getHours());
+    var m = checkTime(d.getMinutes());
+    var s = checkTime(d.getSeconds());
+    var c = document.querySelector('clock');
+    c.innerHTML=[h,m,s].join(':');
+    setTimeout(startTime, 100);
 }
 function getTime() {
 	var d = new Date();
-	var h = checkTime(d.getHours());
-	var m = checkTime(d.getMinutes());
-	var s = checkTime(d.getSeconds());
-	return [h,m,s].join(':');
+    var h = checkTime(d.getHours());
+    var m = checkTime(d.getMinutes());
+    var s = checkTime(d.getSeconds());
+    return [h,m,s].join(':');
 }
 function checkTime(i) {
 	return i < 10 ? '0'+i : i;
 }
+
 function getPage(page){
-	var req = new XMLHttpRequest();
-	req.open("get","https://jason841226.github.io/ESLab1/"+page);
-	req.onload=function(){
-		var content=byId("content");
-		content.innerHTML=this.responseText;
-	}
-	req.send();
+    $(document).ready(function(){
+        $.ajax({
+            url: ("https://jason841226.github.io/ESLab1/"+page),
+            
+            success:function(data){
+                //var content = $($.parseHTML(data, document, true));
+                $('#content').html(data);
+                eval($(data).find("script").text());
+            }
+        });
+        //$.getScript("somescript.js", function(){
+        //    alert("Script loaded and executed.");
+        //});
+        //$("#content").html(htmlobj.responseText);
+    });
 }

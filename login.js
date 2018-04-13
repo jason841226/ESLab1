2f8b02127cf464d86be210f756aae9a8b037c7b3
jason841226 <b03901053@ntu.edu.tw>
2018-04-13 15:05:06 +0800

    Update login.js

--- login.js
+++ login.js
@@ -35,6 +35,6 @@ byId("login_button").addEventListener("click", function(){
   });
 });
 byId("sign_up_button").addEventListener("click", function(){
-  getPage('sign_up.html');
+  window.location.replace('https://jason841226.github.io/ESLab1/sign_up.html');
 });
 
